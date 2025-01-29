fb84f789431f821b03a37fdaaf865058be8583f8
Jonatas Oliveira <jonatasoliveiramont@gmail.com>
2025-01-29 18:53:01 -0300

    Project created

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import db.DB;
+
+public class Main {
+    public static void main(String[] args) {
+
+        DB.getConnection();
+        DB.closeConnection();
+    }
+}
---
+++ src/db/DB.java
@@ -0,0 +1,53 @@
+package db;
+
+import java.sql.*;
+
+public class DB {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/jdbcprojectdao";
+    private static final String USER = "root";
+    private static final String PASS = "12052475";
+
+    private static Connection conn = null;
+
+    public static Connection getConnection() {
+        if (conn == null) {
+            try {
+                conn = DriverManager.getConnection(URL, USER, PASS);
+            } catch (SQLException e) {
+                throw new RuntimeException(e.getMessage());
+            }
+        }
+        return conn;
+    }
+
+    public static void closeConnection() {
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public static void closeStatement(Statement st) {
+        if (st != null) {
+            try {
+                st.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public static void closeResultSet(ResultSet rs) {
+        if (rs != null) {
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
