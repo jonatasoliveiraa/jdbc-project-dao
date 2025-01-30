01fda4270ec68864a72f0b9e310a51393b324706
Jonatas Oliveira <jonatasoliveiramont@gmail.com>
2025-01-30 11:08:42 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,16 @@
 package application;
 
 import entities.Department;
+import entities.Seller;
+
+import java.time.LocalDate;
 
 public class Program {
     public static void main(String[] args) {
 
         Department department = new Department(1, "books");
-        System.out.println(department);
+
+        Seller seller = new Seller(21, "Alex", "alex@gmail.com", LocalDate.now(), 3000.0, department);
+        System.out.println(seller);
     }
 }
